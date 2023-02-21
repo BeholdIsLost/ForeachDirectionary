c618252397b23ffca2eabfda1bdd7e290a6ab70a
opilane <opilane@HP-CZC0359NNB>
2023-02-21 09:24:46 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = new Dictionary<int, string>
+            {
+                {1, "Tallinna Tööstushariduskeskus"},
+                {2, "Tallinna Nõmme Põhikool"},
+                {3, "Ristiku Põhikool"},
+            };
+
+            result.ToList().ForEach(x => Console.WriteLine($"{x.Key} - {x.Value}"));
+        }
+    }
+}
